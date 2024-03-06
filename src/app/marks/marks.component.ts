8fddca6e329b7e6ecdfbe4d8295427b869873ff4
sanjana <sanjanadongre251@gmail.com>
2024-03-06 22:42:34 +0530

    3

---
+++ src/app/marks/marks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marks',
+  standalone: true,
+  imports: [],
+  templateUrl: './marks.component.html',
+  styleUrl: './marks.component.css'
+})
+export class MarksComponent {
+
+}
